7d275fb0dae8a51f94f51b61cd0962ea16d4e442
Hantonny Korrea <hantonny.20@gmail.com>
2022-03-28 23:27:33 -0400

    :sparkles: - ex016: Vamos contar

---
+++ aula14ex/ex016/script.js
@@ -0,0 +1,38 @@
+function contar() {
+    
+    var inicio = document.getElementById('inicio').value
+
+    var i = Number(inicio)
+
+    var fim = document.getElementById('fim').value
+
+    var f = Number(fim)
+
+    var passo = document.getElementById('passo').value
+
+    var p = Number(passo)
+    
+    var res = document.getElementById('res')
+
+    res.innerHTML = ''
+    res.style.fontSize = '1.2rem';
+    if(inicio !== "" && fim !== "") {
+        if(p === 0) {
+            alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+        res.innerHTML += '<p>Contando:</p>'
+        do {
+            
+            res.innerHTML += `${i}`
+            i += p
+            if(i <= f) {
+                res.innerHTML += `👉`
+            } else {
+                res.innerHTML +=`🏁`
+            }
+        } while(i <= f)
+    } else {
+        res.innerHTML += '<p>Impossível contar!</p>'
+    }
+}
